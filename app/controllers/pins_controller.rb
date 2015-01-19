55352c01c081b416d23c4c26e2d2de4a8d4ee5fe
Christopher Leung <chrisleungcl217@gmail.com>
2015-01-19 16:09:18 -0500

    fixed bugs

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,27 +3,22 @@ class PinsController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
-  respond_to :html
-
   def index
-    @pins = Pin.paginate(:per_page => 5, :page => params[:page]).order('created_at DESC')
-    respond_with(@pins)
+    @pins = Pin.paginate(:per_page => 4, :page => params[:page]).order('created_at DESC')
   end
 
   def show
-    respond_with(@pin)
   end
 
   def new
     @pin = current_user.pins.build
-    respond_with(@pin)
   end
 
   def edit
   end
 
   def create
-    @pin = current_user.pins.build(pin_params)
+    @pin = current_user.pins.build(pin_params)   
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
@@ -32,16 +27,20 @@ class PinsController < ApplicationController
   end
 
   def update
-    @pin.update(pin_params)
-    respond_with(@pin)
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
     @pin.destroy
-    respond_with(@pin)
+    redirect_to pins_url
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
     end
@@ -51,6 +50,7 @@ class PinsController < ApplicationController
       redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description, :image)
     end
